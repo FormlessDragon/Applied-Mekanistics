d9d70ef8d45cd24c2c09bf4b99f3ba6c97638eba
vcwdfca <53033260+vcwdfca@users.noreply.github.com>
2026-06-14 20:14:16 +0800

    update dependecies and modid

--- src/main/java/me/ramidzkh/mekae2/Reference.java
+++ src/main/java/me/ramidzkh/mekae2/Reference.java
@@ -2,7 +2,7 @@ package me.ramidzkh.mekae2;
 
 public final class Reference {
 
-    public static final String MOD_ID = "appmek";
+    public static final String MOD_ID = "mekeng";
     public static final String MOD_NAME = "Applied Mekanistics";
     public static final String VERSION = "1.6.0";
 
